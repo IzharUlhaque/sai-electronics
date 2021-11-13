786c2fadb475d681af8bbb8a96f2b816b392aa01
Izhar Ulhaque <codewithizhar@gmail.com>
2021-11-13 19:46:09 +0530

    Add circular category & remove carosul

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ $(document).ready(function(){
   	e.preventDefault();
   	let name = $('#name').val();
   	let email = $('#email').val();
-  	let requirement = $('#requirement').val(); 
-  	let url = `https://wa.me/918275219250?text=Name : ${name}%0aEmail: ${email}%0aI am looking for ${requirement}`
-  	let whatsAppLink = url.replace(/ /g, '%20');
-	  goToURL(whatsAppLink);
+  	let requirement = $('#requirement').val();
+  	if(name === '' || email === ''){
+  		alert('please provide name and email');
+  	} else{
+  		let url = `https://wa.me/918275219250?text=Name : ${name}%0aEmail: ${email}%0aI am looking for ${requirement}`
+  		let whatsAppLink = url.replace(/ /g, '%20');
+	  	goToURL(whatsAppLink);  		
+		}
   });
 });
