0116cb60d0290edfd35a72a65738ac246e22f71c
Izhar Ulhaque <codewithizhar@gmail.com>
2021-11-13 11:50:57 +0530

    Add whats app link on form

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	const goToURL = function(url){
+		  location.href = url;
+	}
+  $("#submit").click(function(e){
+  	e.preventDefault();
+  	let name = $('#name').val();
+  	let email = $('#email').val();
+  	let requirement = $('#requirement').val(); 
+  	let url = `https://wa.me/918275219250?text=Name : ${name}%0aEmail: ${email}%0aI am looking for ${requirement}`
+  	let whatsAppLink = url.replace(/ /g, '%20');
+	  goToURL(whatsAppLink);
+  });
+});
