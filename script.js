3256fdaefc4366ec881dd07226ba4763d41dda04
Izhar Ulhaque <codewithizhar@gmail.com>
2021-11-13 12:07:19 +0530

    Add clear form

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ $(document).ready(function(){
   	let name = $('#name').val();
   	let email = $('#email').val();
   	let requirement = $('#requirement').val(); 
-		name.value='';
-		email.value='';
-		requirement.value='';
   	let url = `https://wa.me/918275219250?text=Name : ${name}%0aEmail: ${email}%0aI am looking for ${requirement}`
   	let whatsAppLink = url.replace(/ /g, '%20');
 	  goToURL(whatsAppLink);
+	  $('#name').value = '';
+	  $('#email').value = '';
+	  $('requirement').value = '';
   });
 });
